25c5bbfd40f5d8581ad6c5a9df597bc070ece5c9
Alquimas <dinizmichel12@gmail.com>
2023-07-17 17:33:01 -0300

    a

--- Hanoi_Tower.hs
+++ Hanoi_Tower.hs
@@ -1,22 +1,25 @@
+-- Faça um algoritmo que, dado um número de pegs e o nome de cada pino, retorne
+-- a lista de movimentos ideal para resolver o quebra cabeça
+--------------------------------------------------------------------------------
 type Peg = String
 type Move = (Peg, Peg)
 
 -- A B C
 -- X, Y - pega a peça de X e põe em Y
---2^n - 1 moves
---1 - A, C 
---2 - A, B | A, C 
---    B, C
---3 - A, C | A, B | C, B | A, C 
---    B, A | B, C | A, C
---4 - A, B | A, C | B, C | A, B | C, A | C, B | A, B | A, C | 
---    B, C | B, A | C, A | B, C | A, B | A, C | B, C
+-- 2^n - 1 moves
+-- 1 - A, C 
+-- 2 - A, B | A, C 
+--     B, C
+-- 3 - A, C | A, B | C, B | A, C 
+--     B, A | B, C | A, C
+-- 4 - A, B | A, C | B, C | A, B | C, A | C, B | A, B | A, C | 
+--     B, C | B, A | C, A | B, C | A, B | A, C | B, C
 --
---    Qual o padrão?
---    Faz o caminho anterior com B e C trocados, i.e., mover as n-1 primeiras 
---    peças para B depois A, C, i.e., mover a peça maior para C
---    Por fim o caminho anterior com A e B trocados, i.e., mover as n-1 
---    primeiras peças para C
+--     Qual o padrão?
+--     Faz o caminho anterior com B e C trocados, i.e., mover as n-1 primeiras
+--     peças para B depois A, C, i.e., mover a peça maior para C
+--     Por fim o caminho anterior com A e B trocados, i.e., mover as n-1
+--     primeiras peças para C
 
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
--- binary_tree.hs
+++ binary_tree.hs
@@ -1,6 +1,9 @@
 data Tree a = Node a (Tree a) (Tree a)
             | Empty
- deriving (Show)
+
+instance (Show a) => Show (Tree a) where
+    show Empty = " 'EMPTY' "
+    show (Node b c d) = " '" ++ (show b) ++ "' (" ++ show c ++ show d ++ ")"
 
 data Tree' a = Node' a (Maybe (Tree' a)) (Maybe (Tree' a))
  deriving (Show)
@@ -8,7 +11,7 @@ data Tree' a = Node' a (Maybe (Tree' a)) (Maybe (Tree' a))
 
  --try to calculate the height of a tree
  --
-height' :: Tree a -> Int
+height' :: (Show a) => Tree a -> Int
 height' Empty = 0
 height' (Node _ (x) (y)) = 1 + max (height' x) (height' y)
 
